cec428f3d6a5a64ac4980f9e1f77f9be6de24fdc
CespSebas <159726485+CespSebas@users.noreply.github.com>
2024-04-15 23:13:01 -0600

    Update JsInformacion.js

--- ProyectoMadeca/JavaScript/JsInformacion.js
+++ ProyectoMadeca/JavaScript/JsInformacion.js
@@ -11,3 +11,27 @@ function iniciarMap(){
 
     //Key AIzaSyDeZja9xleZ_sxLb8FUkgcLva8_70KiWUk
 }
+
+$(document).ready(function () {
+  $('#myForm').submit(function (e) {
+    var name = $('#inputGroup-sizing-default').val();
+    var email = $('#exampleFormControlInput1').val();
+    var error = '';
+
+    if (!name || !email) {
+      error = 'Por favor complete todos los campos.';
+    } else if (!isValidEmail(email)) {
+      error = 'Por favor ingrese un correo electrónico válido.';
+    }
+
+    if (error) {
+      $('.error').html(error);
+      e.preventDefault();
+    }
+  });
+
+  function isValidEmail(email) {
+    var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    return pattern.test(email);
+  }
+});
