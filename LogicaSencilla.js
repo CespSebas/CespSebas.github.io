1b0521d295147554a497c60e145bcd33742c9c20
CespSebas <159726485+CespSebas@users.noreply.github.com>
2024-06-02 23:16:23 -0600

    Update LogicaSencilla.js

--- LogicaSencilla.js
+++ LogicaSencilla.js
@@ -1,26 +1,10 @@
-var check = document.querySelector(".check");
 
-check.addEventListener('click', idioma);
-
-function idioma() {
-
-    let id = check.checked;
-
-    if (id == true) {
-        location.href = "Plantilla Sencilla/MaquetaSencilla.html";
-        
-    } else {
-        location.href = "SencillaEspa.html";
-    }
+function mostrarAlerta() {
+    alert("¡Bienvenidos! Disfruten de nuestro menú");
 }
 
-  function mostrarAlerta() {
-            alert("¡Hola! Este es un mensaje de alerta.");
-        }
-
-        // Mostrar la alerta al cargar la página
-        window.onload = mostrarAlerta;
-
+// Mostrar la alerta al cargar la página
+window.onload = mostrarAlerta;
 
 document.addEventListener('DOMContentLoaded', () => {
     const navbarToggle = document.getElementById('navbarToggle');
